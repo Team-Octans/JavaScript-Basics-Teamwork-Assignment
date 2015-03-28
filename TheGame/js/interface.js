6adb6f5263bab37dc3eb6d4208cc2146a46a0cf6
Svetlozar Kirkov <svetlozark@gmail.com>
2015-03-28 22:47:52 +0200

    added outline to score and time text

--- TheGame/js/interface.js
+++ TheGame/js/interface.js
@@ -23,19 +23,24 @@ function loadGun() {
 
 function drawClock() {
     var clockText,
+    	clockTextClone,
         seconds,
         minutes;
 
     clock = new Date("January 1, 2015 00:02:00");
     
     function updateClock() {
-        stage.removeChild(clockText);
+        stage.removeChild(clockText,clockTextClone);
 
         if (seconds == 0 && minutes == "00") {
-            clockText = new createjs.Text("GAME OVER!", "42px Chunk", "black");
-            clockText.x = 600;
+            clockText = new createjs.Text("GAME OVER!", "32px Chunk", "black");
+            clockText.x = 650;
             clockText.y = stage.canvas.height - 70;
-            stage.addChild(clockText);
+            clockText.outline = 5;
+            clockTextClone = clockText.clone();
+            clockTextClone.outline = false;
+            clockTextClone.color = "white";
+            stage.addChild(clockText,clockTextClone);
         }
         else {
             clock.setSeconds(clock.getSeconds()-1);
@@ -43,10 +48,16 @@ function drawClock() {
             if (seconds < 10) { seconds = "0" + seconds;};
             minutes = clock.getMinutes();
 
-            clockText = new createjs.Text(minutes + ":" + seconds, "42px Chunk", "black");
+            clockText = new createjs.Text("Time: " + minutes + ":" + seconds, "32px Chunk", "black");
             clockText.x = 700;
             clockText.y = stage.canvas.height - 70;
-            stage.addChild(clockText);
+            clockText.outline = 5;
+
+            clockTextClone = clockText.clone();
+            clockTextClone.outline = false;
+            clockTextClone.color = "white";
+            stage.addChild(clockText,clockTextClone);
+
         }
     }
     setInterval(updateClock,1000);
@@ -55,13 +66,19 @@ function drawClock() {
 function drawScore() {
     var playerScore = 0; // for now it is here
     var playerScoreText;
+    var playerScoreTextClone;
 
     function updateScore() {
         stage.removeChild(playerScoreText);
-        playerScoreText = new createjs.Text("Score: " + playerScore, "42px Chunk", "black");
-        playerScoreText.x = 350;
+        playerScoreText = new createjs.Text("Score: " + playerScore, "32px Chunk", "black");
+        playerScoreText.x = 250;
         playerScoreText.y = stage.canvas.height - 70;
+        playerScoreText.outline = 5;
+        playerScoreTextClone = playerScoreText.clone();
+        playerScoreTextClone.outline = false;
+        playerScoreTextClone.color = "white";
         stage.addChild(playerScoreText);
+        stage.addChild(playerScoreTextClone);
     }
 
     setInterval(updateScore,1000);
